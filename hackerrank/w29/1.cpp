7751d750abefad30dd6216ea7d3b2372c9dd1430
hsharsh <praveenharsh01@gmail.com>
2017-04-08 22:07:16 +0530

    8th April, 2017

---
+++ hackerrank/w29/1.cpp
@@ -0,0 +1,69 @@
+/*****************************************************************************************
+You should enjoy the little detours...
+Becase that's where you'll find the things
+more important than what you want...
+
+Version	:	1.5
+Author	:	Harshwardhan Praveen
+*****************************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pi 			3.141592653593
+#define MIN 		-1000000001
+#define MAX 		1000000001
+#define EPS 		0.000000001
+
+#define cns 		ios_base::sync_with_stdio(false)
+#define DEBUG(x) 	cout << '>' << #x << ':' << x << endl
+#define REP(i,n) 	for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) 	for(ll i=(a);i<(b);i++)
+#define DFOR(i,a,b) for(ll i=(a);i>(b);i--)
+#define pb 			push_back
+#define mp 			make_pair
+#define all(v) 		v.begin(),v.end()
+#define rall(v) 	v.rbegin(),v.rend()
+#define vi 			vector<int>
+#define vl 			vector<ll>
+#define vii 		vector<vector<int> >
+#define vll 		vector<vector<ll> >
+#define vs 			vector<string>
+#define si 			set<int>
+#define pii 		pair<int,int>
+#define pll			pair<ll,ll>
+#define F 			first
+#define S 			second
+#define ll 			long long
+#define ull 		unsigned long long
+
+ll power_modulo(ll base, ll exp, ll mod){
+	ll result = 1;
+	base %= mod;
+	while(exp > 0){
+		if(exp%2 == 1)
+			result = (result*base) % mod;
+		exp = exp>>1;
+		base = (base*base) % mod;
+	}
+	return result;
+}
+
+int main(){
+	int year;
+	cin>>year;
+	if(year<1918){
+		if(year%4==0)
+			cout<<"12.09."<<year<<endl;
+		else
+			cout<<"13.09."<<year<<endl;
+	}
+	else if(year==1918){
+		cout<<"26.09.1918"<<endl;
+	}
+	else{
+		if((year%400==0)||(year%4==0 && year%100!=0))
+			cout<<"12.09."<<year<<endl;
+		else
+			cout<<"13.09."<<year<<endl;
+	}		
+}
---
+++ kickstart/practice/A.cpp
@@ -0,0 +1,76 @@
+/*****************************************************************************************
+You should enjoy the little detours...
+Becase that's where you'll find the things
+more important than what you want...
+
+Version	:	1.5
+Author	:	Harshwardhan Praveen
+*****************************************************************************************/
+#include <bits/stdc++.h>
+using namespace std;
+
+#define pi 			3.141592653593
+#define MIN 		-1000000001
+#define MAX 		1000000001
+#define EPS 		0.000000001
+
+#define cns 		ios_base::sync_with_stdio(false)
+#define DEBUG(x) 	cout << '>' << #x << ':' << x << endl
+#define REP(i,n) 	for(ll i=0;i<(n);i++)
+#define FOR(i,a,b) 	for(ll i=(a);i<(b);i++)
+#define DFOR(i,a,b) for(ll i=(a);i>(b);i--)
+#define pb 			push_back
+#define mp 			make_pair
+#define all(v) 		v.begin(),v.end()
+#define rall(v) 	v.rbegin(),v.rend()
+#define vi 			vector<int>
+#define vl 			vector<ll>
+#define vii 		vector<vector<int> >
+#define vll 		vector<vector<ll> >
+#define vs 			vector<string>
+#define si 			set<int>
+#define pii 		pair<int,int>
+#define pll			pair<ll,ll>
+#define F 			first
+#define S 			second
+#define ll 			long long
+#define ull 		unsigned long long
+
+ll power_modulo(ll base, ll exp, ll mod){
+	ll result = 1;
+	base %= mod;
+	while(exp > 0){
+		if(exp%2 == 1)
+			result = (result*base) % mod;
+		exp = exp>>1;
+		base = (base*base) % mod;
+	}
+	return result;
+}
+
+int main(){
+	freopen("input.in", "r", stdin);
+	freopen("output.out", "w", stdout);
+	int t, n;
+	string s,ans;
+	cin>>t;
+	FOR(i,1,t+1){
+		int m=0;
+		cin>>n;
+		while(n--){
+			int curr=0;
+			set<char> letters;
+			cin>>s;
+			REP(j,s.size()){
+				if(isalpha(s[i]))
+					letters.insert(s[i]);
+			}
+			curr=letters.size();
+			if(curr > m){
+				m = curr;
+				ans=s;
+			}
+		}
+		cout<<"Case #"<<i<<": "<<ans<<endl;
+	}
+}
